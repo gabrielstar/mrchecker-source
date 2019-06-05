2a7c24bc9629fe6bf17d3390861b6f86cac5bd0e
GSTARCZE <gabriel.starczewski@capgemini.com>
2019-06-05 13:52:34 +0200

    removing unnecessary code

--- mrchecker-framework-modules/pipelines/seed.groovy
+++ mrchecker-framework-modules/pipelines/seed.groovy
@@ -2,28 +2,19 @@ class JobConfig {
     def URL
     def oldItemsNumKeep = '3'
     def oldItemsDaysKeep = '1'
-    protected String jobName
-    protected String scriptPath
-    protected String credentialsId
-    def performanceJobs = []
-
-    String toString() {
-        return "$URL:$oldItemsNumKeep:$oldItemsDaysKeep"
-    }
-}
-
-class PerformanceJobConfig extends JobConfig{
-
+    def jobName
+    def scriptPath
+    def credentialsId
 }
 
 String folderSource = '''
 folder(':folder:') {
-    description('Repository jobs for Zensus Projects')
+    description('Test Jobs for MrChecker')
 }
-
 '''
+
 String dslScriptTemplate = '''
-multibranchPipelineJob(':folder:/:jobName:.:browser:.:env:') {
+multibranchPipelineJob(':folder:/:jobName:') {
     description(":description:")
     displayName(":jobName:")
     branchSources{
@@ -91,23 +82,6 @@ pipelineJob(':folder:/:jobName:') {
 }
 '''
 
-String dslTestPipelineTemplate = '''
-    pipelineJob(':folder:/:jobName:.:env:'){
-      description(":description:")
-      definition{
-        cpsFlowDefinition{
-          script("""
-                    def buildJob
-                    def failureStatus = 'FAILURE'
-                    node(){
-                        :jobList:
-                    }
-                 """)
-          sandbox(true)
-        }
-      }
-}
-'''
 
 String view = '''
             listView('tests/:name:') {
@@ -132,19 +106,17 @@ String view = '''
 
 //performance feature & tests put inside because number of jobs per view grew too large
 public enum JOB_TYPES {
-    FEATURE("feature"), REGRESSION("regression"), STANDALONE("standalone"), PIPELINE("pipeline"),
-    PERFORMANCE("performance"),PERFORMANCE_REGRESSION("performance/regression"), PERFORMANCE_FEATURE("performance/feature")
+    FEATURE("feature"), REGRESSION("regression"), STANDALONE("standalone")
     String folder
     String rootFolder = "tests"
-
     private JOB_TYPES(String folder) {
         this.folder = "$rootFolder/$folder"
     }
 }
 
-//envs and envs templates for where they are dynamically generated
+//in case we ever add run-time tests
 public enum ENVIRONMENTS {
-    DEV("DEV__ENV__01"), DEV2("DEV__ENV__02"),PINT1("PINT1"), PINT2("PINT2")
+    DEV("DEV")
     final String env
 
     private ENVIRONMENTS(String env) {
@@ -152,34 +124,19 @@ public enum ENVIRONMENTS {
     }
 }
 final String mainFolder = "tests"
-final List browsers = ["firefox", "ie"]
-List pipelineBrowsers = browsers
-pipelineBrowsers.removeAll(["ie"]) //jobs for this browser will not make part of test pipeline
-//we cannot use "-" here because static methods are disallowed on jenkins
-
-//generations exclusions
-def excludedEnvironments = []
-def excludedEnvironmentsForRegression = []
-//some projects use common envs e.g. datenportal-gateway -> datenportal
-def replaceMapEnvs = ["-gateway_":""]
 List dslScripts = []
 def credentialsId = 'CORP-TU'
 
-def getRegressionJobFor(String projectName,String browser,String env,String branch, boolean isPerformance) {
+def getRegressionJobFor(String projectName,String env,String branch) {
     def jobRelativePath
     def jobRoot
     def downstreamJob
     def jobDescription
-    if(isPerformance){
-        jobRoot = "tests/performance/regression/"
-        jobDescription = "Performance tests for $projectName"
-        jobRelativePath = "${projectName.toLowerCase()}.${replaceVariablesForEnvironments(env,projectName)}/$branch"
 
-    }else{
-        jobRoot = "tests/regression/"
-        jobDescription = "Functional Tests Regression with $browser for $projectName"
-        jobRelativePath = "${projectName.toLowerCase()}.$browser.${replaceVariablesForEnvironments(env,projectName)}/$branch"
-    }
+    jobRoot = "tests/regression/"
+    jobDescription = "Functional Tests Regression for $projectName"
+    jobRelativePath = "${projectName.toLowerCase()}.${replaceVariablesForEnvironments(env,projectName)}/$branch"
+
 
     downstreamJob = "buildJob = build job: '$jobRoot$jobRelativePath',propagate:false"
 
@@ -200,7 +157,7 @@ def getRegressionJobFor(String projectName,String browser,String env,String bran
 }
 
 
-def getJobForConfig(String jobTemplate, JobConfig jobConfig, JOB_TYPES jobType, String description, String browser, String env) {
+def getJobForConfig(String jobTemplate, JobConfig jobConfig, JOB_TYPES jobType, String description, String env) {
     jobConfig['oldItemsNumKeep'] = jobConfig['oldItemsNumKeep'] ?: 1
     jobConfig['oldItemsDaysKeep'] = jobConfig['oldItemsDaysKeep'] ?: 1
 
@@ -233,13 +190,9 @@ def getJobForConfig(String jobTemplate, JobConfig jobConfig, JOB_TYPES jobType, 
             replaceAll(':includes:', includes).
             replaceAll(':excludes:', excludes).
             replaceAll(':trigger:', trigger).
-            replaceAll(':browser:', browser).
             replaceAll(':env:', env)
 }
 
-String replaceVariablesForEnvironments(String env, String projectKey){
-    return env.replace("_ENV_", projectKey)
-}
 
 Map<String, JobConfig> repoJobConfigs = [:]
 
@@ -254,57 +207,30 @@ repoJobConfigs.put('Checker',
 
 
 //generates functional feature jobs for all branches, with default environment, testers can change
-def generateFeatureJobConfigs(String repoName, JobConfig repoConfig, def dslScriptTemplate, def browsers){
+def generateFeatureJobConfigs(String repoName, JobConfig repoConfig, def dslScriptTemplate){
     List<JobConfig> configs = []
-    browsers.each { browser ->
-        def description = "This is the feature job for project ${repoName} for browser ${browser}. By default it runs all tests that are tagged with branch name e.g. @SAF-203. All feature branches get their own jobs. They need to be triggered manually."
-        configs.add(
-                getJobForConfig(dslScriptTemplate, repoConfig, JOB_TYPES.FEATURE, description, browser, "")
-        )
-    }
+
+    def description = "This is the feature job for project ${repoName} for browser ${browser}. By default it runs all tests that are tagged with branch name e.g. @SAF-203. All feature branches get their own jobs. They need to be triggered manually."
+    configs.add(
+            getJobForConfig(dslScriptTemplate, repoConfig, JOB_TYPES.FEATURE, description, "")
+    )
+
     configs
 }
 
-def generateRegressionJobConfigs(String repoName, JobConfig repoConfig, def dslScriptTemplate, def browsers, def ENVIRONMENTS, def excludedEnvironmentsForRegression){
+def generateRegressionJobConfigs(String repoName, JobConfig repoConfig, def dslScriptTemplate, def ENVIRONMENTS){
     def configs = []
-    browsers.each { browser ->
-        ENVIRONMENTS.each {
-            //regression jobs for develop, for each browser and environment, every 60 mins
-            description = "This is the regression job for project ${repoName} for browser ${browser} and environment ${it.env}. By default it runs all tests that are tagged with @regression tag. Only develop gets regression job by default. "
-            description += "They run regularly twice a day with cron job."
-            def env = replaceVariablesForEnvironments(it.env, repoConfig["jobName"])
-            if(!(env in excludedEnvironmentsForRegression)) {
-                println "ENV: $env"
-                configs.add(getJobForConfig(dslScriptTemplate, repoConfig, JOB_TYPES.REGRESSION, description, browser, env))
-            }
-        }
-    }
-    configs
-}
 
-def generateTestPipelinesJobConfigs(String repoName, JobConfig repoConfig, def dslTestPipelineTemplate, def dslPerformanceTemplate,  def browsers, def ENVIRONMENTS, def excludedEnvironmentsForRegression){
-    def configs = []
-    ENVIRONMENTS.each {
-        description = "Test Pipelines for integration with Production Line"
-        def env = replaceVariablesForEnvironments(it.env, repoConfig["jobName"])
-        if(!(env in excludedEnvironmentsForRegression)) {
-            configs.add(
-                    getJobForConfig(dslTestPipelineTemplate, repoConfig, JOB_TYPES.PIPELINE, description, "", env)
-                            .replaceAll(":jobList:",
-                            browsers.collect { browser ->
-                                getRegressionJobFor(repoConfig['jobName'], browser, it.env, "develop", false)
-                            }
-                            .join("\n ")
-                                    .plus("\n")
-                                    .plus(
-                                    repoConfig.performanceJobs.collect { def performanceRepoConfig->
-                                        getRegressionJobFor(performanceRepoConfig['jobName'], "", env, "develop", true)
-                                    }.join("\n")
-                            )
-                    )
-            )
-        }
+    ENVIRONMENTS.each { env->
+        //regression jobs for develop, for each browser and environment, every 60 mins
+        description = "This is the regression job for project ${repoName}  and environment ${it.env}. By default it runs all tests that are tagged with @regression tag. Only develop gets regression job by default. "
+        description += "They run regularly twice a day with cron job."
+
+        println "ENV: $env"
+        configs.add(getJobForConfig(dslScriptTemplate, repoConfig, JOB_TYPES.REGRESSION, description, env))
+
     }
+
     configs
 }
 
@@ -341,10 +267,10 @@ node() {
         repoJobConfigs.each { String repoName, JobConfig repoConfig ->
             println "Generating functional tests feature jobs configs: "
             //feature jobs for all branches, with default environment, testers can change
-            dslScripts += generateFeatureJobConfigs(repoName, repoConfig, dslScriptTemplate,browsers)
+            dslScripts += generateFeatureJobConfigs(repoName, repoConfig, dslScriptTemplate)
 
             println "Generating functional tests regression jobs configs: "
-            dslScripts += generateRegressionJobConfigs(repoName,repoConfig,dslScriptTemplate,browsers,ENVIRONMENTS, excludedEnvironmentsForRegression)
+            dslScripts += generateRegressionJobConfigs(repoName,repoConfig,dslScriptTemplate,ENVIRONMENTS)
 
             println "Generating standalone jobs configs"
             //stand-alone jobs
